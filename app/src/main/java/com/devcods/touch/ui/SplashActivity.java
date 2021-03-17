b2963d9b60234aea46c21f4cf872ef5013ea69db
Shabeen <shabeen@infobreez.com>
2021-03-17 13:03:09 +0530

    - Almost Completed Home Page Design .

--- app/src/main/java/com/devcods/touch/ui/SplashActivity.java
+++ app/src/main/java/com/devcods/touch/ui/SplashActivity.java
@@ -2,7 +2,9 @@ package com.devcods.touch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.devcods.touch.R;
 
@@ -12,5 +14,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_splash );
+        new Handler(  ).postDelayed( () -> {
+            startActivity( new Intent( SplashActivity.this, LoginActivity.class ));
+            finish();
+        },2500);
     }
 }
